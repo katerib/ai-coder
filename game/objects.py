dff90ed83ef705db745cf12a3538c7450c91d290
Kat <91296845+katerib@users.noreply.github.com>
2023-07-20 18:01:51 -0400

    Update map & objects JSON; update player class and text adventure class for gameplay

--- game/objects.py
+++ game/objects.py
@@ -3,4 +3,51 @@ class Objects:
         self.objects = data
 
     def get_object(self, object_name):
-        return self.objects[object_name]
+        """Returns the object data based on the given object name"""
+        return self.objects.get(object_name)
+
+    def examine_object(self, object_name):
+        """Prints the description of the specified object"""
+        object_data = self.get_object(object_name)
+        if object_data:
+            print("Name:", object_data["name"])
+            print("Description:", object_data["description"])
+        else:
+            print("Object not found.")
+
+    def use_object(self, object_name):
+        """Performs an action when the specified object is used"""
+        object_data = self.get_object(object_name)
+        if object_data:
+            print("Using", object_data["name"])
+            # Implement the desired action for using the object
+        else:
+            print("Object not found.")
+
+    def take_object(self, object_name):
+        """Adds the specified object to the player's inventory"""
+        object_data = self.get_object(object_name)
+        if object_data:
+            if object_data["equipped"]:
+                print("You have already taken the", object_data["name"])
+            else:
+                object_data["equipped"] = True
+                print("You took the", object_data["name"])
+        else:
+            print("Object not found.")
+
+    def is_object_obtained(self, object_name):
+        """Checks if the specified object has been obtained"""
+        object_data = self.get_object(object_name)
+        if object_data:
+            return object_data["equipped"]
+        else:
+            return False
+
+    def get_equipped_objects(self):
+        """Returns a list of equipped objects"""
+        equipped_objects = []
+        for object_name, object_data in self.objects.items():
+            if object_data["equipped"]:
+                equipped_objects.append(object_name)
+        return equipped_objects
